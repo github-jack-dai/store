05da64c1104853f0811afaeed3897f13448c7baa
dyx <jackdai20200615@163.com>
2022-01-22 22:33:56 +0800

    store项目的第三次提交，完成了登录和注册功能的编写

--- src/main/java/com/cy/store/controller/BaseController.java
+++ src/main/java/com/cy/store/controller/BaseController.java
@@ -1,16 +1,33 @@
 package com.cy.store.controller;
 
 
-import com.cy.store.service.ex.InsertException;
-import com.cy.store.service.ex.ServiceException;
-import com.cy.store.service.ex.UsernameDuplicateException;
+import com.cy.store.service.ex.*;
 import com.cy.store.util.JsonResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpSession;
 
 /** 控制器类的基类 */
 public class BaseController {
     /** 操作成功的状态码 */
     public static final int OK = 200;
+    /**
+     * 从HttpSession对象中获取uid
+     * @param session HttpSession对象
+     * @return 当前登录的用户的id
+     */
+    protected final Integer getUidFromSession(HttpSession session) {
+        return Integer.valueOf(session.getAttribute("uid").toString());
+    }
+
+    /**
+     * 从HttpSession对象中获取用户名
+     * @param session HttpSession对象
+     * @return 当前登录的用户名
+     */
+    protected final String getUsernameFromSession(HttpSession session) {
+        return session.getAttribute("username").toString();
+    }
 
     /** @ExceptionHandler用于统一处理方法抛出的异常 */
     @ExceptionHandler({ServiceException.class})
@@ -18,9 +35,18 @@ public class BaseController {
         JsonResult<Void> result = new JsonResult<Void>(e);
         if (e instanceof UsernameDuplicateException) {
             result.setState(4000);
+            result.setMessage("用户名被占用的异常");
         } else if (e instanceof InsertException) {
             result.setState(5000);
+            result.setMessage("插入数据时产生未知的异常");
+        }else if (e instanceof PasswordNotMatchException){
+            result.setState(5001);
+            result.setMessage("用户名的密码错误的异常");
+        }else if (e instanceof UserNotFoundException){
+            result.setState(5002);
+            result.setMessage("用户数据不存在的异常");
         }
         return result;
     }
+
 }
--- src/main/java/com/cy/store/controller/UserController.java
+++ src/main/java/com/cy/store/controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
+
 /** 处理用户相关请求的控制器类 */
 @RestController
 @RequestMapping("users")
@@ -44,4 +46,18 @@ public class UserController extends BaseController {
         return result;
     }
     */
+
+    @RequestMapping("login")
+    public JsonResult<User> login(String username, String password, HttpSession session) {
+        // 调用业务对象的方法执行登录，并获取返回值
+        User data = userService.login(username, password);
+        //登录成功后，将uid和username存入到HttpSession中
+        session.setAttribute("uid", data.getUid());
+        session.setAttribute("username", data.getUsername());
+        // System.out.println("Session中的uid=" + getUidFromSession(session));
+        // System.out.println("Session中的username=" + getUsernameFromSession(session));
+
+        // 将以上返回值和状态码OK封装到响应结果中并返回
+        return new JsonResult<User>(OK, data);
+    }
 }
